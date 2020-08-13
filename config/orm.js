840112f214049ca389c5a4a94872acc4e4024461
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-08-12 22:06:54 -0400

    minor renaming to functions

--- config/orm.js
+++ config/orm.js
@@ -32,7 +32,7 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 const orm = {
-  all: function (tableInput, cb) {
+  selectAll: function (tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function (err, result) {
       if (err) {
@@ -41,7 +41,7 @@ const orm = {
       cb(result);
     });
   },
-  create: function (table, cols, vals, cb) {
+  insertOne: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -61,7 +61,7 @@ const orm = {
       cb(result);
     });
   },
-  update: function (table, objColVals, condition, cb) {
+  updateOne: function (table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
